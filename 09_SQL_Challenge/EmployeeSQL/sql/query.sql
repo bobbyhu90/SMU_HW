db10132f426d1a0bb5e7a8aaa7f7767c97042f93
Zizhen Hu <zhenhu47@gmail.com>
2021-06-15 18:16:56 -0500

    added repo

--- 09_SQL_Challenge/EmployeeSQL/sql/query.sql
+++ 09_SQL_Challenge/EmployeeSQL/sql/query.sql
@@ -16,7 +16,6 @@ JOIN departments AS d ON m.dept_no=d.dept_no
 JOIN employees AS e ON m.emp_no=e.emp_no
 ORDER BY dept_no;
 
-
 -- 4employee data with employee number, last name, first name, and department name
 SELECT e.emp_no, e.last_name, e.first_name, d.dept_name FROM employees AS e
 JOIN dept_emp ON e.emp_no=dept_emp.emp_no
